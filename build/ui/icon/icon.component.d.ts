ab7f4409e1938adc4d08aa92553748673a81f82e
IrulArif <masterirul@gmail.com>
2019-09-19 00:57:50 +0700

    fix

---
+++ build/ui/icon/icon.component.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+interface IconProps {
+    source: any;
+    name: string;
+    size?: number;
+    color?: string;
+}
+declare const _default: ({ source, name, size, color }: IconProps) => JSX.Element;
+export default _default;
---
+++ build/ui/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var icon_component_1 = require("./icon/icon.component");
+exports.Icon = icon_component_1.default;
