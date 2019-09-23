7167c73b411bae64347672eeba3fa049204734fe
Rizky <rizky@andromedia.co.id>
2019-09-23 20:27:26 +0700

    fix

--- ui/index.ts
+++ ui/index.ts
@@ -1,2 +1,3 @@
 export { default as Icon } from './icon';
+export { default as Input } from './input/index';
 export { default as Select } from './select';
--- ui/input/index.web.tsx
+++ ui/input/index.web.tsx
@@ -4,6 +4,7 @@ import { Input, InputProps } from 'react-native-ui-kitten';
 export default (props: InputProps) => {
   return (
     <Input
+      className='cactiva-input'
       textStyle={{
         minHeight: 32
       }}
