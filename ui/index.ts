9c69107ce21151ee8ee686813b03d33fb7eb858e
IrulArif <masterirul@gmail.com>
2019-10-23 05:06:06 +0700

    fix

--- ui/index.ts
+++ ui/index.ts
@@ -5,12 +5,25 @@ const { width, height } = Dimensions.get("window");
 const guidelineBaseWidth = 350;
 const guidelineBaseHeight = 680;
 
-const scale = size => (width / guidelineBaseWidth) * size;
-const verticalScale = size => (height / guidelineBaseHeight) * size;
-const moderateScale = (size, factor = 0.5) =>
+const scale = (size: number) => (width / guidelineBaseWidth) * size;
+const verticalScale = (size: number) => (height / guidelineBaseHeight) * size;
+const moderateScale = (size: number, factor: number = 0.5) =>
   size + (scale(size) - size) * factor;
+const uuid = (prefix: string = randString()) =>
+  `${prefix}-${new Date().getTime()}${Math.floor(
+    10000000 + Math.random() * 90000000
+  )}`;
 
-export { scale, verticalScale, moderateScale };
+const randString = (length: number = 5) => {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const charactersLength = characters.length;
+  let result = "";
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+export { scale, verticalScale, moderateScale, uuid, randString };
 export { default as Icon } from "./icon";
 export { default as Input } from "./input/index";
 export { default as Select } from "./select";
